1b10285908117e5b5d1dd59631d7d5e771a8607d
Caio Henrique Rombaldo <chrstream@hotmail.com>
2021-02-24 21:52:39 -0300

    Survey
    Add survey and prepare to jest

--- node.js/api/src/routes.ts
+++ node.js/api/src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import { SurveyController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController'
+import { SurveyRepository } from './repositories/SurveyRepository';
 
 const router = Router();
 
 const userController = new UserController();
+const surveyController = new SurveyController();
 
 router.post("/users", userController.create);
+router.post("/survey", surveyController.create);
+router.get("/survey", surveyController.show);
 
 export { router };
