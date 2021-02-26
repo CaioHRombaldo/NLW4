fb07b924a659a1682db1ceaa2a42b557290290e0
Caio Henrique Rombaldo <chrstream@hotmail.com>
2021-02-26 10:03:32 -0300

    email sender

--- node.js/api/src/routes.ts
+++ node.js/api/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { SendMailController } from './controllers/SendMailController';
 import { SurveyController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController'
 import { SurveyRepository } from './repositories/SurveyRepository';
@@ -7,9 +8,11 @@ const router = Router();
 
 const userController = new UserController();
 const surveyController = new SurveyController();
+const sendMailController = new SendMailController();
 
 router.post("/users", userController.create);
 router.post("/survey", surveyController.create);
 router.get("/survey", surveyController.show);
 
+router.post("/sendmail", sendMailController.execute);
 export { router };
